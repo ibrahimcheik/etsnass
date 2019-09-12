67bac0bfa370eef0f2f05b7be0238cada0f823f8
Amadou Moussa Ibrahim <lecheikibrane@gmail.com>
2019-09-12 09:01:29 +0100

    Update sitemap.php

--- sitemap.php
+++ sitemap.php
@@ -1,18 +1,27 @@
-<?php
-    
-    $connect = mysql_connect("localhost","root","","etsnass");
+<?php 
+//sitemap.php
+$connect = mysqli_connect("localhost", "root", "", "etsnass");
 
-    $query = "SELECT PageName FROM tbl_pages ";
+$query = "SELECT PageName FROM tbl_pages";
 
-    $result = mysqli_query($connect, $query);
+$result = mysqli_query($connect, $query);
 
-    $base_url = "http://localhost/etsnass/";
+$base_url = "http://localhost/etsnass/";
 
-    header("Content-type: application/xml; charset="utf-8");
+header("Content-Type: application/xml; charset=utf-8");
 
-    echo '<?xml version="1.0" encoding="UTF-8" standalone="no"?>'.PHP_EOL;
+echo '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL; 
 
-    echo '<urlset xmlns="https://www.sitemaps.org/shemas/sitemap/0.9 " ''
+echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">' . PHP_EOL;
 
+while($row = mysqli_fetch_array($result))
+{
+ echo '<url>' . PHP_EOL;
+ echo '<loc>'.$base_url. $row["page_url"] .'/</loc>' . PHP_EOL;
+ echo '<changefreq>daily</changefreq>' . PHP_EOL;
+ echo '</url>' . PHP_EOL;
+}
 
- ?>
+echo '</urlset>' . PHP_EOL;
+
+?>
